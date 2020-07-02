294c41cd0ba0370252994003d804aace2ae6645a
glendell03 <dellbringino@gmail.com>
2020-07-02 17:12:41 +0800

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './header.styles.scss'
-import { ReactComponent as Logo } from '../../assets/logo.svg'
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
-const Header = () => (
-    <div className='header'>
-        <Link className="logo-container" to="/">
-            <Logo className="logo" />
-        </Link>
-        <div className="nav-links">
-            <Link className="links" to="/shop">
-                SHOP
-            </Link>
-            <Link className="links" to="/contact">
-                CONTACT
-            </Link>
-        </div>
+import { auth } from '../../firebase/firebase.utils'
+
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="nav-links">
+      <Link className="links" to="/shop">
+        SHOP
+      </Link>
+      <Link className="links" to="/size-guide">
+        SIZE GUIDE
+      </Link>
+      <Link className="links" to="/about-us">
+        ABOUT US
+      </Link>
+      <Link className="links" to="/contact-us">
+        CONTACT US
+      </Link>
+      {
+        currentUser ? 
+        <div className="links" onClick={() => auth.signOut()}>SIGN OUT</div>
+        :
+        <Link className="links" to='/signin'>SIGN IN</Link>
+      }
     </div>
-)
+  </div>
+);
 
 export default Header;
