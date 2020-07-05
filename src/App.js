b28162c02bb081c3e946e56f6eca473d8c0162e4
glendell03 <dellbringino@gmail.com>
2020-07-05 18:25:16 +0800

    implemented firebase utils, included ability to store authenticated user into to our firestore database

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import "./assets/base.scss";
-import "./App.css";
+import "./App.scss";
 
 import HomePage from "./pages/homepage/HomePage";
 import ShopPAge from "./pages/shop/shop";
 import Header from "./components/header/header";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -18,22 +17,38 @@ class App extends React.Component {
     };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.setState);
+          
+        });
+        
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPAge} />
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,37 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+import {
+  SearchOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+  UserDeleteOutlined,
+} from "@ant-design/icons";
+import { Squash as Hamburger } from "hamburger-react";
 
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from "../../firebase/firebase.utils";
+import NavLinks from "./nav-links";
 
-const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="nav-links">
-      <Link className="links" to="/shop">
-        SHOP
-      </Link>
-      <Link className="links" to="/size-guide">
-        SIZE GUIDE
-      </Link>
-      <Link className="links" to="/about-us">
-        ABOUT US
-      </Link>
-      <Link className="links" to="/contact-us">
-        CONTACT US
-      </Link>
-      {
-        currentUser ? 
-        <div className="links" onClick={() => auth.signOut()}>SIGN OUT</div>
-        :
-        <Link className="links" to='/signin'>SIGN IN</Link>
-      }
-    </div>
-  </div>
-);
+const Header = ({ currentUser }) => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  return (
+    <nav role="navigation">
+      <div className="header">
+        <div className="burger-menu" id="burger-menu">
+          <Hamburger
+            rounded
+            size={20}
+            toggled={menuActive}
+            toggle={setMenuActive}
+          />
+        </div>
+        <div
+          className="isClicked"
+          onClick={() => setMenuActive(false)}
+        >
+          <Link className="logo-container" to="/">
+            <Logo className="logo" />
+          </Link>
+          <NavLinks show={menuActive} />
+          <div className="btn-links-container" id="btn-links-container">
+            <SearchOutlined className="btn-links" id="btn-links" />
+            {currentUser ? (
+              <div onClick={() => auth.signOut()}>
+                <UserDeleteOutlined className="btn-links" id="btn-links" />
+              </div>
+            ) : (
+              <Link to="/signin">
+                <UserOutlined className="btn-links" id="btn-links" />
+              </Link>
+            )}
+            <ShoppingOutlined className="btn-links" id="btn-links" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
